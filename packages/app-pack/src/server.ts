8f1e76615fb80339dfea288f4762d4925e3e468d
Neer Friedman <neerfri@gmail.com>
2024-01-23 16:58:20 +0200

    sleep before calling server.close()

--- packages/app-pack/src/server.ts
+++ packages/app-pack/src/server.ts
@@ -1,11 +1,15 @@
 import crypto from "crypto";
 import Fastify, { FastifyInstance, FastifyServerOptions } from "fastify";
+import { sleep } from "@unkjd/utils";
 
 type ServerOptions = {
   host: string;
   port: number;
+  gracefulShutdownDelay?: number;
   fastifyOptions: FastifyServerOptions;
 };
+
+type ServerConfig = Required<ServerOptions>;
 
 const fastifyDefaultOptions: FastifyServerOptions = {
   logger: {
@@ -16,27 +20,36 @@ const fastifyDefaultOptions: FastifyServerOptions = {
 };
 
 export class Server {
-  readonly options: ServerOptions;
-  readonly fastify: FastifyInstance;
+  readonly config: ServerConfig;
+  readonly fastify: FastifyInstance & { shuttingDown?: boolean };
+  readonly abortController = new AbortController();
 
   constructor(options: ServerOptions) {
-    this.options = {
+    this.config = this.configWithDefaults(options);
+    this.fastify = Fastify(this.config.fastifyOptions);
+    this.fastify.decorate("shuttingDown", false);
+  }
+
+  async start() {
+    const { host, port } = this.config;
+    await this.fastify.listen({ host, port });
+  }
+
+  async stop(reason?: string) {
+    this.fastify.shuttingDown = true;
+    await sleep(this.config.gracefulShutdownDelay);
+    await this.fastify.close();
+  }
+
+  private configWithDefaults(options: ServerOptions): ServerConfig {
+    return {
       ...options,
+      gracefulShutdownDelay: options.gracefulShutdownDelay ?? 1000,
       fastifyOptions: this.fastifyOptionsWithDefaults(options.fastifyOptions),
     };
-    this.fastify = Fastify(this.options.fastifyOptions);
   }
 
   private fastifyOptionsWithDefaults(fastifyOptions: FastifyServerOptions): FastifyServerOptions {
     return { ...fastifyDefaultOptions, ...fastifyOptions };
-  }
-
-  async start() {
-    const { host, port } = this.options;
-    await this.fastify.listen({ host, port });
-  }
-
-  async stop() {
-    await this.fastify.close();
   }
 }
